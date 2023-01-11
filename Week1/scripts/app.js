e470343a6d6937a37253034515bd89661c929b6d
Justin Collier <justin.collier@dcmail.ca>
2023-01-10 19:00:14 -0500

    Yarn Config

---
+++ Week1/scripts/app.js
@@ -0,0 +1,15 @@
+"user strict";
+
+//IIFE - Immediately Invoked Function Expression
+//AKA - Anonymous Self-Executing Function
+
+(function(){
+
+    function Start()
+    {
+        console.log("App Started!")
+    }
+    window.addEventListener("load", Start)
+
+
+})();

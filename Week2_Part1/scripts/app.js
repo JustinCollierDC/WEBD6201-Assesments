93306d9e021c521e65badae34a90324a2ff57523
Justin Collier <justin.collier@dcmail.ca>
2023-01-17 02:49:51 -0500

    Week 2 - Page Layout

--- Week2_Part1/scripts/app.js
+++ Week2_Part1/scripts/app.js
@@ -2,14 +2,59 @@
 
 //IIFE - Immediately Invoked Function Expression
 //AKA - Anonymous Self-Executing Function
-
-(function(){
-
+(function()
+{
     function Start()
     {
         console.log("App Started!")
+
+
     }
     window.addEventListener("load", Start)
+}
+)();
 
+function DisplayHomePage()
+{
+    let AboutUsButton = document.getElementById("AboutUsButton");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "index.html"
+    });
+}
 
-})();
+function DisplayAboutPage()
+{
+    let AboutUsButton = document.getElementById("AboutUsButton");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "about.html"
+    });
+}
+
+function DisplayProductsPage()
+{
+    let AboutUsButton = document.getElementById("AboutUsButton");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "products.html"
+    });
+}
+
+function DisplayServicesPage()
+{
+    let AboutUsButton = document.getElementById("AboutUsButton");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "services.html"
+    });
+}
+
+function DisplayContactPage()
+{
+    let AboutUsButton = document.getElementById("AboutUsButton");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "contact.html"
+    });
+}

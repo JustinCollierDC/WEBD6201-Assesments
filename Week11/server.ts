b53da61741c0b5a6040e8624371cd5d5af1e993e
Justin Collier <justin.collier@dcmail.ca>
2023-03-21 18:24:09 -0400

    Week 11 - ICE 5

--- Week11/server.ts
+++ Week11/server.ts
@@ -1,17 +1,45 @@
 // Code Sourced From: https://nodejs.org/en/docs/guides/getting-started-guide
 
+"use strict"
+
 //const http = require('http');
 import http from 'http';
+import fs from 'fs';
+import mime from 'mime-types';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+let lookup = mime.lookup;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+//const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) =>
+{
+    let path : string = req.url as string;
+    console.log(__dirname);
+    console.log(path);
+
+    if(path === "/" || path === "/home")
+    {
+        path = "index.ejs";
+    }
+
+    let mime_type : string = lookup(path.substring(1)) as string;
+    console.log("mime-type: " + mime_type);
+
+    fs.readFile(__dirname, function(err, data)
+    {
+        if(err)
+        {
+            res.writeHead(404);
+            res.end("Error 404 - File Not Found " + err.message);
+            return;
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200);
+        res.end(data);
+    });
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at ${port}/`);
 });
